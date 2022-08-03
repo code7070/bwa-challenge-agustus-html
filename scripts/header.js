c5d8e411ee823decc579ea1c6c2934249c26ce87
Jojo <aditya.abdul@weekendinc.com>
2022-08-04 00:00:57 +0700

    add header adjustment

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+let inputSearch = document.getElementById("searchBox");
+let groupSearch = document.getElementById("groupSearch");
+
+function setFocusSearch() {
+  groupSearch.classList.add("active");
+}
+
+function unsetFocusSearch() {
+  groupSearch.classList.remove("active");
+}
