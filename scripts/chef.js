655cfdf8189d48d17409439049268b877c622bb4
Jojo <aditya.abdul@weekendinc.com>
2022-08-04 12:39:09 +0700

    add onclick chef toggle

--- scripts/chef.js
+++ scripts/chef.js
@@ -2,3 +2,25 @@ let chefPhoto = "./assets/images/chef/phoenix-satcheup.jpg";
 let elChefPhoto = document.getElementById("chefPhoto");
 
 elChefPhoto.src = chefPhoto;
+
+function clickChef(el) {
+  // const otherEl = document.getElementsByClassName("cs--selector");
+
+  if (!el.classList.contains("active")) {
+    const otherEl = document.querySelectorAll(".cs--selector");
+    console.log({ otherEl });
+    if (otherEl.forEach((el) => el.classList.remove("active")));
+    el.classList.add("active");
+    elChefPhoto.src = chefPhoto;
+  }
+}
+
+function clickChef1(el) {
+  chefPhoto = "./assets/images/chef/phoenix-satcheup.jpg";
+  clickChef(el);
+}
+
+function clickChef2(el) {
+  chefPhoto = "./assets/images/chef/renatta-moeloek.jpg";
+  clickChef(el);
+}
